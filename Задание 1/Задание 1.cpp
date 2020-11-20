acc20403d63688eeebb027b20d11218df77ed378
Роман Курилов <radiancejh@gmail.com>
2020-11-20 14:38:04 +0300

    Добавьте файлы проекта.

---
+++ Задание 1/Задание 1.cpp
@@ -0,0 +1,21 @@
+﻿// Задание 1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(0, "");
+	int g;
+	cout << "Введите ваше число: ";
+	cin >> g;
+	cout << "Числа, на которые делится ваше число: " << endl;
+	for (int i = 1; i <= 1000; i++)
+	{
+		if (g % i == 0)
+		{
+			cout << i << endl;
+		}
+	}
+	system("pause");
+}
