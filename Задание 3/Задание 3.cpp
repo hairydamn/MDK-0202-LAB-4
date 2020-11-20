1f0875aee0db299af2691ce1d83ebf36aae50257
Роман Курилов <radiancejh@gmail.com>
2020-11-20 14:47:21 +0300

    +3 задание

---
+++ Задание 3/Задание 3.cpp
@@ -0,0 +1,23 @@
+﻿// Задание 3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	setlocale(0, "");
+	double x;
+	cout << "Введите значение x: " << endl;
+	cin >> x;
+	double n;
+	cout << "Введите значение n: " << endl;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		x = ((pow(x, 2)) / 2) + ((pow(x, 3)) / 3) - ((pow(x, 4)) / 4) + (pow(-1, n + 1) * pow(x, n)) / n - log(1 + x);
+	}
+	cout << "Значение логарифма: " << x << endl;
+
+	system("pause");
+}
